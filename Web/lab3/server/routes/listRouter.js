cb6a38bc216965bf1adef7383cd44487a0dbd06f
Alex <myhailovalex@gmail.com>
2022-06-26 19:41:18 +0300

    Final

--- Web/lab3/server/routes/listRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const hobbyController = require('../controllers/hobbyController');
-
-router.post('/', hobbyController.create);
-router.get('/', hobbyController.getAll);
-router.get('/:id', hobbyController.getOne);
-
-module.exports = router
